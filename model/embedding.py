a5a67bee96aec684a19bf1e8b002ced4a681d6ce
GGOURAB SWAIN <gourabswain526@gmail.com>
2026-01-17 12:33:03 +0530

    distilbert enhanced

--- model/embedding.py
+++ model/embedding.py
@@ -27,7 +27,7 @@ def get_embedding(text):
 
     return embeddings.numpy()
 
-text = "I have exams next week and I feel stressed"
-embedding = get_embedding(text)
+# text = "I have exams next week and I feel stressed"
+# embedding = get_embedding(text)
 
-print(embedding)
+# print(embedding)
